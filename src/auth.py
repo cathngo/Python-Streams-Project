4961c1177064ab90638fa5a1da2b0c47fbf7061c
Minwook Han <z5259887@ad.unsw.edu.au>
2021-09-24 01:58:47 +1000

    Finished feature auth_register_v1 and created auth_registed_helper

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,6 @@
 from src.data_store import data_store
 from src.error import InputError
+from src.auth_register_helper import check_email, check_duplicate_email, check_password, check_name, create_handle
 
 def auth_login_v1(email, password):
     return {
@@ -7,6 +8,36 @@ def auth_login_v1(email, password):
     }
 
 def auth_register_v1(email, password, name_first, name_last):
+    store = data_store.get()
+
+    # Checks user details for validity
+    check_email(email)
+    check_duplicate_email(email, store)
+    check_password(password)
+    check_name(name_first)
+    check_name(name_last)
+
+    auth_user_id = len(store['users'])
+
+    # First user to register becomes the streams owner
+    is_streams_owner = True if (auth_user_id == 0) else False
+    
+    # Creates unique handle
+    handle = create_handle(name_first, name_last, store)
+    
+    # Registers user into store
+    store['users'].append(
+        {
+            'auth_user_id': auth_user_id,
+            'email': email,
+            'password': password,
+            'name_first': name_first, 
+            'name_last': name_last,
+            'handle': handle,
+            'is_streams_owner': is_streams_owner,
+        }
+    )
+    
     return {
-        'auth_user_id': 1,
+        'auth_user_id': auth_user_id
     }
---
+++ src/auth_register_helper.py
@@ -0,0 +1,70 @@
+#auth_register_v1 helper functions
+import re
+from src.error import InputError
+
+# Checks user's email to see whether it is valid or not
+def check_email(email):
+    # Regular expression for validating an email
+    regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
+    
+    # Email length cannot be 0 or greater than 254
+    if len(email) == 0 or len(email) > 254:
+        raise InputError
+    
+    # Checks if invalid email format
+    if not re.fullmatch(regex, email):
+        raise InputError
+
+# Checks if an email address is already being used by another user
+def check_duplicate_email(email, store):
+    for user in store['users']:
+        if user['email'] == email:
+            raise InputError
+
+# Checks if user's password less than 6 characters (invalid)
+def check_password(password):
+    if len(password) < 6:
+        raise InputError
+
+# Checks if length of user's first and last name is not between 1 and 50 characters inclusive (invalid)
+def check_name(name):
+    if len(name) < 1 or len(name) > 50:
+        raise InputError
+
+# Check data_store to see if generated handle is already in use
+# Return True if handle is already in use, otherwise return False
+def check_duplicate_handle(handle, store):
+    for user in store['users']:
+        if user['handle'] == handle:
+            return True
+    return False
+
+# Generates a unique handle for the user
+def create_handle(name_first, name_last, store):
+    # Concatenates lowercase-only alphanumeric (a-z0-9) first name and last name.
+    handle = ''
+    for character in name_first:
+        if character.islower() or character.isdigit():
+            handle += character
+
+    for character in name_last:
+        if character.islower() or character.isdigit():
+            handle += character
+
+    # If length of handle is longer than 20 characters, it is cut off at 20 characters
+    if len(handle) > 20:
+        handle = handle[:20]
+
+    '''
+    If the handle is once again taken, append the concatenated names with the smallest
+    number (starting from 0) that forms a new handle that isn't already taken.
+    '''
+    if check_duplicate_handle(handle, store) == True:
+        handle += '0'
+
+    i = 1
+    while check_duplicate_handle(handle, store) == True:
+        handle[-1] = i
+        i += 1
+
+    return handle
