bd0195c3549ee585652ca51e408a783b602853b7
z5255617 <z5255617@cse.unsw.edu.au>
2021-10-01 23:44:44 +1000

    Fixed minor errors in pytests

--- tests/channel_details_test.py
+++ tests/channel_details_test.py
@@ -6,19 +6,20 @@ from src.channel import channel_details_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
+#user id data
 @pytest.fixture
 def test_u_id():
     clear_v1()
     return auth_register_v1('validemail@gmail.com', '123abc!@#', 'Sam', 'Smith')
 
-
+#channel id data
 @pytest.fixture
 def test_c_id(test_u_id):
     return channels_create_v1(test_u_id['auth_user_id'], 'Alpaca', True) 
 
-#check channel id exists
-def test_nonexistent_channel_id(test_u_id, test_c_id):
-    invalid_channel_id = 100
+#check when no channels created
+def test_nonexistent_channel_id(test_u_id):
+    invalid_channel_id = 1000
     with pytest.raises(InputError):
         channel_details_v1(test_u_id['auth_user_id'], invalid_channel_id)
 
@@ -28,13 +29,13 @@ def test_invalid_channel_id(test_u_id, test_c_id):
     with pytest.raises(InputError):
         channel_details_v1(test_u_id['auth_user_id'], invalid_channel_id)
 
-#check channel id valid but auth id invalid
+#check channel id valid but auth id is not part of channel
 def test_unauthorised_member(test_u_id, test_c_id):
     invalid_u_id = test_u_id['auth_user_id'] + 1
     with pytest.raises(AccessError):
         channel_details_v1(invalid_u_id, test_c_id['channel_id'])  
 
-#check both channel id and auth id invalid
+#check when both channel id and auth id invalid
 def test_empty_store():
     clear_v1()
     with pytest.raises(AccessError):
@@ -63,15 +64,12 @@ def test_correct_channel_details(test_u_id, test_c_id):
     assert details['is_public'] == True
 
 #check returns correct channel details given multiple channels a user has created
-def test_multiple_channels():
-    clear_v1()
-    #register first user
-    u_id_zero = auth_register_v1('emailzero@gmail.com', '123abc!@#', 'Sam', 'Smith')
+def test_multiple_channels(test_u_id):
     #create multiple channels with first user
-    c_id_zero = channels_create_v1(u_id_zero['auth_user_id'], 'Channel Zero', True) 
-    c_id_one = channels_create_v1(u_id_zero['auth_user_id'], 'Channel One', False) 
-    c_id_two = channels_create_v1(u_id_zero['auth_user_id'], 'Channel Two', True) 
-    #test details of channel two is correct
-    details = channel_details_v1(u_id_zero['auth_user_id'], c_id_two['channel_id'])
+    c_id_zero = channels_create_v1(test_u_id['auth_user_id'], 'Channel Zero', True) 
+    c_id_one = channels_create_v1(test_u_id['auth_user_id'], 'Channel One', True) 
+    c_id_two = channels_create_v1(test_u_id['auth_user_id'], 'Channel Two', False) 
+    #test returns details of channel two
+    details = channel_details_v1(test_u_id['auth_user_id'], c_id_two['channel_id'])
     assert details['name'] == 'Channel Two'
-    
+    assert details['is_public'] == False
