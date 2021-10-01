2bfe168c21440689ffef6d8b346143aba8468472
z5255617 <z5255617@cse.unsw.edu.au>
2021-10-01 21:34:07 +1000

    Added more tests using channel details

--- tests/channels_create_test.py
+++ tests/channels_create_test.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.error import InputError, AccessError
 from src.channels import channels_create_v1
+from src.channel import channel_details_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
@@ -11,6 +12,11 @@ def test_u_id():
     clear_v1()
     return auth_register_v1('validemail@gmail.com', '123abc!@#', 'Sam', 'Smith')
 
+#check function returns a valid channel id
+def test_returns_channel_id(test_u_id):
+    c_id = channels_create_v1(test_u_id['auth_user_id'], 'Alpaca', False)
+    assert c_id != None
+
 #check if name is less than 1 character or greater than 20 characters
 def test_channels_invalid_name(test_u_id):
     with pytest.raises(InputError):
@@ -18,7 +24,6 @@ def test_channels_invalid_name(test_u_id):
     with pytest.raises(InputError):
         channels_create_v1(test_u_id['auth_user_id'], 'nameLongerThanTwentyCharacters', True) 
          
-
 #check auth_id exists
 def test_nonexistent_auth_id(test_u_id):
     #check empty user list
@@ -36,3 +41,30 @@ def test_unique_channel_id(test_u_id):
     channel_id_one = channels_create_v1(test_u_id['auth_user_id'], 'channel_name', True)
     channel_id_two = channels_create_v1(id_two['auth_user_id'], 'channel_name', True)
     assert channel_id_one != channel_id_two
+
+#check creator of channel is the owner
+def test_owner(test_u_id):
+    c_id = channels_create_v1(test_u_id['auth_user_id'], 'Alpaca', False)
+    details = channel_details_v1(test_u_id['auth_user_id'], c_id['channel_id'])
+    assert details['owner_members'][0]['u_id'] == test_u_id['auth_user_id']
+
+#check name is added
+def test_owner(test_u_id):
+    c_id = channels_create_v1(test_u_id['auth_user_id'], 'Alpaca', False)
+    details = channel_details_v1(test_u_id['auth_user_id'], c_id['channel_id'])
+    assert details['owner_members'][0]['u_id'] == test_u_id['auth_user_id']
+    assert details['all_members'][0]['u_id'] == test_u_id['auth_user_id']
+
+#check name is added
+def test_owner(test_u_id):
+    c_id = channels_create_v1(test_u_id['auth_user_id'], 'Alpaca', False)
+    details = channel_details_v1(test_u_id['auth_user_id'], c_id['channel_id'])
+    assert details['name'] == 'Alpaca'
+
+#check correct is_public value added
+def test_owner(test_u_id):
+    c_id = channels_create_v1(test_u_id['auth_user_id'], 'Alpaca', False)
+    details = channel_details_v1(test_u_id['auth_user_id'], c_id['channel_id'])
+    assert details['is_public'] == False
+
+
