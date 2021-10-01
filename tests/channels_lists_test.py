536edc2c50a884092a645fc9beeb196268f0f595
Alejandro Stevan <z5295949@cse.unsw.edu.au>
2021-10-02 04:03:19 +1000

    Fixtures added

--- tests/channels_lists_test.py
+++ tests/channels_lists_test.py
@@ -5,6 +5,19 @@ from src.channels import channels_create_v1, channels_list_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
+
+@pytest.fixture
+def clear_register_create_channels():
+    clear_v1()
+    #register users
+    user_id1 = auth_register_v1('test1@gmail.com', 'test321', 'Jack', 'Smith')
+    user_id2 = auth_register_v1('other1@gmail.com', 'test4321', 'Willis', 'Posa')
+    #create the channels
+    channels_create_v1(user_id1['auth_user_id'], 'is_member1', True)
+    channels_create_v1(user_id1['auth_user_id'], 'is_member2', True)
+    channels_create_v1(user_id1['auth_user_id'], 'is_member3', True)
+    return [user_id1, user_id2]
+
 def test_no_channel_joined():
     clear_v1()
     
@@ -13,10 +26,12 @@ def test_no_channel_joined():
     joined_channels = channels_list_v1(user_id1['auth_user_id'])
     assert len(joined_channels['channels']) == 0
     
+def test_no_channel_joined2():
+    clear_v1()
     #check when there is other channels
-    user_id2 = auth_register_v1('other1@gmail.com', 'test321', 'Willis', 'Posa')
-    channels_create_v1(user_id2['auth_user_id'], 'channel1', True)  
-    channels_create_v1(user_id2['auth_user_id'], 'channel2', True)
+    user_id1 = auth_register_v1('test1@gmail.com', 'test321', 'Jack', 'Smith')
+    user_id2 = auth_register_v1('other1@gmail.com', 'test4321', 'Willis', 'Posa')
+    channels_create_v1(user_id2['auth_user_id'], 'is_member1', True)
     joined_channels = channels_list_v1(user_id1['auth_user_id'])
     assert len(joined_channels['channels']) == 0
 
@@ -25,30 +40,15 @@ def test_auth_invalid_2():
     with pytest.raises(AccessError):
         channels_list_v1('non_existent')
         
-def test_multiple_channels_list():
-    clear_v1()
-    user_id1 = auth_register_v1('test1@gmail.com', 'test321', 'Jack', 'Smith')
-    user_id2 = auth_register_v1('other1@gmail.com', 'test321', 'Willis', 'Posa')
-    
-    # check when user_id1 is member in multiple channels
-    channels_create_v1(user_id2['auth_user_id'], 'is_not_a_member', True)  
-    channels_create_v1(user_id1['auth_user_id'], 'is_member1', True)
-    channels_create_v1(user_id1['auth_user_id'], 'is_member2', True)
-    channels_create_v1(user_id1['auth_user_id'], 'is_member3', True)
-    joined_channels = channels_list_v1(user_id1['auth_user_id'])
+def test_multiple_channels_list(clear_register_create_channels):
+    user_id1 = clear_register_create_channels
+    joined_channels = channels_list_v1(user_id1[0]['auth_user_id'])
     assert len(joined_channels['channels']) == 3
     
     
-def test_private_channels_list():
-    clear_v1()
-    user_id1 = auth_register_v1('test1@gmail.com', 'test321', 'Jack', 'Smith')
-    user_id2 = auth_register_v1('other1@gmail.com', 'test321', 'Willis', 'Posa')
-    
+def test_private_channels_list(clear_register_create_channels):
+    users = clear_register_create_channels
     # check when user_id1 is member in multiple channels including private ones
-    channels_create_v1(user_id2['auth_user_id'], 'is_not_a_member', True)  
-    channels_create_v1(user_id1['auth_user_id'], 'is_member1', True)
-    channels_create_v1(user_id1['auth_user_id'], 'is_member2', False)
-    channels_create_v1(user_id1['auth_user_id'], 'is_member3', False)
-    joined_channels = channels_list_v1(user_id1['auth_user_id'])
-    assert len(joined_channels['channels']) == 3
-    
+    channels_create_v1(users[0]['auth_user_id'], 'is_member4', False)
+    joined_channels = channels_list_v1(users[0]['auth_user_id'])
+    assert len(joined_channels['channels']) == 4
--- tests/channels_listsall_test.py
+++ tests/channels_listsall_test.py
@@ -5,6 +5,14 @@ from src.channels import channels_create_v1, channels_listall_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
+@pytest.fixture
+def clear_register_users():
+    clear_v1()
+    #register users
+    user_id1 = auth_register_v1('test1@gmail.com', 'test321', 'Jack', 'Smith')
+    user_id2 = auth_register_v1('other1@gmail.com', 'test4321', 'Willis', 'Posa')
+    return [user_id1, user_id2]
+
 def test_no_channels():
     clear_v1()
     #check when there is no channels
@@ -17,29 +25,23 @@ def test_auth_invalid():
     with pytest.raises(AccessError):
         channels_listall_v1('non_existent')
     
-def test_multiple_channels_listsall():
-    clear_v1()
-    user_id1 = auth_register_v1('test1@gmail.com', 'test321', 'Jack', 'Smith')
-    user_id2 = auth_register_v1('other1@gmail.com', 'test321', 'Willis', 'Posa')
-    
+def test_multiple_channels_listsall(clear_register_users):
+    users = clear_register_users
     # check multiple public channels with different users
-    channels_create_v1(user_id2['auth_user_id'], 'member', True)  
-    channels_create_v1(user_id1['auth_user_id'], 'member1', True)
-    channels_create_v1(user_id1['auth_user_id'], 'member2', True)
-    channels_create_v1(user_id1['auth_user_id'], 'member3', True)
-    all_channels = channels_listall_v1(user_id1['auth_user_id'])
+    channels_create_v1(users[1]['auth_user_id'], 'member', True)  
+    channels_create_v1(users[0]['auth_user_id'], 'member1', True)
+    channels_create_v1(users[0]['auth_user_id'], 'member2', True)
+    channels_create_v1(users[0]['auth_user_id'], 'member3', True)
+    all_channels = channels_listall_v1(users[0]['auth_user_id'])
     assert len(all_channels['channels']) == 4
     
-def test_private_channels_list():
-    clear_v1()
-    user_id1 = auth_register_v1('test1@gmail.com', 'test321', 'Jack', 'Smith')
-    user_id2 = auth_register_v1('other1@gmail.com', 'test321', 'Willis', 'Posa')
-    
+def test_private_channels_list(clear_register_users):
+    users = clear_register_users
     # check multiple channels including private ones
-    channels_create_v1(user_id2['auth_user_id'], 'test1', True)  
-    channels_create_v1(user_id1['auth_user_id'], 'test2', True)
-    channels_create_v1(user_id2['auth_user_id'], 'test3', False)
-    channels_create_v1(user_id1['auth_user_id'], 'test4', False)
-    all_channels = channels_listall_v1(user_id1['auth_user_id'])
+    channels_create_v1(users[1]['auth_user_id'], 'test1', True)  
+    channels_create_v1(users[0]['auth_user_id'], 'test2', True)
+    channels_create_v1(users[1]['auth_user_id'], 'test3', False)
+    channels_create_v1(users[0]['auth_user_id'], 'test4', False)
+    all_channels = channels_listall_v1(users[0]['auth_user_id'])
     assert len(all_channels['channels']) == 4
     
