bd158414166a571b60a67f27a956b098873ac482
Diwa <z5308320@cse.unsw.edu.au>
2021-10-02 22:48:42 +1000

    Adding fixtures

--- tests/channel_join_test.py
+++ tests/channel_join_test.py
@@ -6,47 +6,45 @@ from src.channel import channel_details_v1, channel_join_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
-# Checks if channel exists in channel list
-def test_invalid_channel_id():
+#user id data
+@pytest.fixture
+def u_id():
     clear_v1()
-    u_id = auth_register_v1('valid@gmail.com', 'abcde123*', 'Snoop', 'Dogg')
+    return auth_register_v1('valid@gmail.com', 'abcde123*', 'Snoop', 'Dogg')
+
+#channel id data
+@pytest.fixture
+def c_id(u_id):
+    return channels_create_v1(u_id['auth_user_id'], 'Rappers', True)  
+
+# Checks if channel exists in channel list
+def test_invalid_channel_id(u_id):
     invalid_channel_id = 124
     with pytest.raises(InputError):
         channel_join_v1(u_id['auth_user_id'], invalid_channel_id)
         
 # Checks if channel exists in channel list
-def test_invalid_channel_id2():
-    clear_v1()
-    u_id = auth_register_v1('valid@gmail.com', 'abcde123*', 'Snoop', 'Dogg')
-    c_id = channels_create_v1(u_id['auth_user_id'], 'Rappers', True) 
+def test_invalid_channel_id2(u_id, c_id):
     invalid_channel_id = c_id['channel_id'] + 1
     with pytest.raises(InputError):
         channel_join_v1(u_id['auth_user_id'], invalid_channel_id)
 
-# Checks to see ifuser is already a member of the channel
-def test_existing_member():
-    clear_v1()
-    u_id = auth_register_v1('valid@gmail.com', 'abcde123*', 'Snoop', 'Dogg')
-    c_id = channels_create_v1(u_id['auth_user_id'], 'Rappers', True) 
+# Checks to see if user is already a member of the channel
+def test_existing_member(u_id, c_id):
     u_id2 = auth_register_v1('valid2@gmail.com', 'abcdef123*', 'Kanye', 'Dogg')
     channel_join_v1(u_id2['auth_user_id'], c_id['channel_id'])
     with pytest.raises(InputError):
         channel_join_v1(u_id2['auth_user_id'], c_id['channel_id'])
 
 # If channel is private and user is not global owner check if join is prevented
-def test_invalid_private_join():
-    clear_v1()
-    u_id = auth_register_v1('valid@gmail.com', 'abcde123*', 'Snoop', 'Dogg')
+def test_invalid_private_join(u_id):
     c_id = channels_create_v1(u_id['auth_user_id'], 'Rappers', False) 
     u_id2 = auth_register_v1('valid2@gmail.com', 'abcdef123*', 'Drake', 'Dogg')
     with pytest.raises(AccessError):
         channel_join_v1(u_id2['auth_user_id'], c_id['channel_id'])
 
 # Check if new member has been added to the member list
-def test_member_update():
-    clear_v1()
-    u_id = auth_register_v1('valid@gmail.com', 'abcde123*', 'Snoop', 'Dogg')
-    c_id = channels_create_v1(u_id['auth_user_id'], 'Rappers', True) 
+def test_member_update(u_id, c_id):
     u_id2 = auth_register_v1('valid2@gmail.com', 'abcdef123*', 'Drake', 'Dogg')
     channel_join_v1(u_id2['auth_user_id'], c_id['channel_id'])
     details = channel_details_v1(u_id['auth_user_id'], c_id['channel_id']) 
