82aa3ffde18b3a1fb1eccfcc60cdc528b58444b4
z5255617 <z5255617@cse.unsw.edu.au>
2021-10-18 05:22:21 +1100

    Added success http tests

--- tests/http_user_profile_sethandle_test.py
+++ tests/http_user_profile_sethandle_test.py
@@ -30,8 +30,6 @@ def test_short_handle_str():
     r = requests.put(config.url + 'user/profile/sethandle/v1', json={'token': user_token['token'], 'handle_str': ''})  
     assert r.status_code == 400
 
-
-
 #check input error if handle_str contains non alphanumeric characters
 def test_non_alphanumeric_handle():
     requests.delete(config.url + 'clear/v1')
@@ -69,3 +67,37 @@ def test_invalid_token_invalid_handle():
     r = requests.put(config.url + 'user/profile/sethandle/v1', json={'token': invalid_token, 'handle_str': ''})   
     assert r.status_code == 403   
   
+#check updated handle successfully for one user
+def test_updated_handle():
+    requests.delete(config.url + 'clear/v1')
+    user1 = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail@gmail.com', 'password': '123abc!@#', 'name_first': 'Sam', 'name_last': 'Smith'})
+    user2 = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail2@gmail.com', 'password': '123abc!@#', 'name_first': 'Samantha', 'name_last': 'Bottle'})
+    user1_token = user1.json()
+    user2_token = user2.json()
+    requests.put(config.url + 'user/profile/sethandle/v1', json={'token': user1_token['token'], 'handle_str': 'updateFirst'})  
+    requests.put(config.url + 'user/profile/sethandle/v1', json={'token': user2_token['token'], 'handle_str': 'updateSecond'})  
+    r1 = requests.get(config.url + 'user/profile/v1', params={'token': user1_token['token'], 'u_id': user1_token['auth_user_id']})
+    r2 = requests.get(config.url + 'user/profile/v1', params={'token': user2_token['token'], 'u_id': user2_token['auth_user_id']})
+    user1_details = r1.json()
+    user2_details = r2.json()
+    assert user1_details['handle_str'] == 'updateFirst'
+    assert user2_details['handle_str'] == 'updateSecond'
+
+#check can update same handle multiple times
+def test_update_multiple():
+    requests.delete(config.url + 'clear/v1')
+    user = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail@gmail.com', 'password': '123abc!@#', 'name_first': 'Sam', 'name_last': 'Smith'})
+    user_token = user.json()
+    requests.put(config.url + 'user/profile/sethandle/v1', json={'token': user_token['token'], 'handle_str': 'updateFirst'})  
+    requests.put(config.url + 'user/profile/sethandle/v1', json={'token': user_token['token'], 'handle_str': 'updateTwo'})  
+    r2 = requests.get(config.url + 'user/profile/v1', params={'token': user_token['token'], 'u_id': user_token['auth_user_id']})
+    details = r2.json()
+    assert details['handle_str'] == 'updateTwo'
+
+#check returns empty dictionary
+def test_empty_dictoinary():
+    requests.delete(config.url + 'clear/v1')
+    user = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail@gmail.com', 'password': '123abc!@#', 'name_first': 'Sam', 'name_last': 'Smith'})
+    user_token = user.json()
+    r = requests.put(config.url + 'user/profile/sethandle/v1', json={'token': user_token['token'], 'handle_str': 'updateFirst'})  
+    assert r.json() == {}
