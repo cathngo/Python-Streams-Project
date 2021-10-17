01e9c826881cdf55c509ec452ca54b9175652b4f
Minwook Han <z5259887@ad.unsw.edu.au>
2021-10-18 00:32:35 +1100

    Added http tests for dm/list/v1

---
+++ tests/dm_list_http_tests.py
@@ -0,0 +1,79 @@
+import requests
+from src import config
+
+def test_dm_list_works():
+    '''
+    Check if dm/list/v1 returns expected output
+    '''
+    requests.delete(config.url + 'clear/v1')
+    r1 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'user1@email.com',
+        'password': 'user1password',
+        'name_first': 'Kanye',
+        'name_last': 'Yeezus',
+    })
+    r2 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'user2@email.com',
+        'password': 'user2password',
+        'name_first': 'Kanye',
+        'name_last': 'East',
+    })
+    payload1 = r1.json()
+    payload2 = r2.json()
+
+    requests.post(config.url + 'dm/create/v1', json={
+        'token': payload1['token'],
+        'u_ids': [payload2['auth_user_id']],
+    })
+    requests.post(config.url + 'dm/create/v1', json={
+        'token': payload1['token'],
+        'u_ids': [],
+    })
+    
+    r3 = requests.post(config.url + 'dm/list/v1', json={
+        'token': payload1['token']
+    })
+    payload3 = r3.json()
+    assert len(payload3['dms']) == 2
+
+def test_no_dms():
+    '''
+    Check when user has 0 dms
+    '''
+    requests.delete(config.url + 'clear/v1')
+    r1 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'user1@email.com',
+        'password': 'user1password',
+        'name_first': 'Kanye',
+        'name_last': 'Yeezus',
+    })
+    payload1 = r1.json()
+
+    r2 = requests.post(config.url + 'dm/list/v1', json={
+        'token': payload1['token'],
+    })
+    payload2 = r2.json()
+    assert len(payload2['dms']) == 0
+
+def test_invalid_token():
+    '''
+    Check if invalid token is passed as input
+    '''
+    requests.delete(config.url + 'clear/v1')
+    r1 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'user1@email.com',
+        'password': 'user1password',
+        'name_first': 'Kanye',
+        'name_last': 'Yeezus',
+    })
+    payload1 = r1.json()
+
+    requests.post(config.url + 'dm/create/v1', json={
+        'token': payload1['token'],
+        'u_ids': [],
+    })
+
+    r2 = requests.post(config.url + 'dm/list/v1', json={
+        'token': 'invalidtoken',
+    })
+    assert r2.status_code == 403
