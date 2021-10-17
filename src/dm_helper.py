8ba9930ae836d2b5acb0723c597ad6f2fc953ba0
Minwook Han <z5259887@ad.unsw.edu.au>
2021-10-18 03:07:55 +1100

    Added more helper functions

--- src/dm_helper.py
+++ src/dm_helper.py
@@ -1,4 +1,5 @@
 from src.channels_invite_helper import check_u_id_exists
+from src.error import InputError, AccessError
 
 def check_valid_u_id_list(u_ids, store):
     '''
@@ -44,3 +45,40 @@ def generate_dm_names(u_ids, store, owner_id):
     # Convert to a string
     dm_name = ', '.join(new_dm_list)
     return dm_name
+
+def check_dm_id_exists(dm_id, store):
+    '''
+    Checks in the data store if the given dm_id exists
+    Returns the DM if it exists
+    '''
+    for dm_iter in store['dm']:
+        if dm_iter['dm_id'] == dm_id:
+            return dm_iter
+    raise InputError(description='dm_id does not exist')
+
+def check_user_in_dm(u_id, dm_id, store):
+    '''
+    Checks if the user is a member of the given dm_id
+    '''
+    for dm_iter in store['dm']:
+        if dm_iter['dm_id'] == dm_id:
+            for user in dm_iter['members']:
+                if user['u_id'] == u_id:
+                    return
+    raise AccessError(description='authorised user is not a member of the DM')
+
+def obtain_user_details(u_id, store):
+    '''
+    Returns a dictionary containing user details
+    Assumes u_id is a valid and existing id in data store
+    '''
+    for user in store['users']:
+        if user['u_id'] == u_id:
+            user_details_temp = {
+                'u_id': user['u_id'],
+                'email': user['email'],
+                'name_first': user['name_first'], 
+                'name_last': user['name_last'],
+                'handle_str': user['handle_str'],
+            }
+            return user_details_temp
