6a2063d9606935684df3042d930b9fcafbe60d9a
z5361577 <z5361577@cse.unsw.edu.au>
2021-10-23 00:31:14 +1100

    Some style changes to channel and dm messages. Added extra helper functions in channel_messages_helper for send_dm&channel. Added messages/send and messages/senddm to server. Added backend function file for these send messages routes. Added tests for these routes.

--- src/channel_messages_helper.py
+++ src/channel_messages_helper.py
@@ -26,9 +26,16 @@ def messages_pagination(len_message_channel_dm, start):
     #goes through the messages in a 0 - 50, 50 - 100, 100 - 150, in pagination 
     #manner, always starting from the first index and then continuing on the 
     #following call
- 
+
     return {
         'end': end, 
         'len_message_channel_dm': len_message_channel_dm, 
         'page_length': page_length,
     }
+
+def check_message_is_right_character_length(message_length): 
+    if message_length < 1:
+        raise InputError("Message is less than one character")
+    elif message_length > 1000:
+        raise InputError("Message is over 1000 character") 
+    return
--- tests/message_send_channel_http_test.py
+++ tests/message_send_channel_http_test.py
@@ -4,8 +4,11 @@ import json
 from src import config
 from src.other import clear_v1
 import jwt
+from tests.message_send_dm_http_test import (
+    test_check_messagge_ids_are_not_the_same_for_different_channel_and_dm_message_send
+)
 
-def test_invalid_channel_id_channel_messages():
+def test_invalid_channel_id_message_send():
     requests.delete(config.url + 'clear/v1')
     invalid_channel_id = 1000
     user = requests.post(config.url + 'auth/register/v2', json={
@@ -15,14 +18,14 @@ def test_invalid_channel_id_channel_messages():
         'name_last': 'Smith'
     })
     user_token = user.json()
-    resp = requests.get(config.url + 'channel/messages/v2', params={
+    resp = requests.post(config.url + 'message/send/v1', params={
         'token': user_token['token'],
         'channel_id': invalid_channel_id,
-        'start': 0
+        'message': "hello"
     })
     assert resp.status_code == 400
 
-def test_unauthorised_user_channel_messages():
+def test_unauthorised_user_message_send():
     requests.delete(config.url + 'clear/v1')
     #create user and channel with token
     user1 = requests.post(config.url + 'auth/register/v2', json={
@@ -47,14 +50,14 @@ def test_unauthorised_user_channel_messages():
         'name_last': 'Smith'
     })
     unauthorised_user = user2.json()
-    resp = requests.get(config.url + 'channel/details/v2', params={
+    resp = requests.post(config.url + 'message/send/v1', params={
         'token': unauthorised_user['token'], 
         'channel_id': channel_id['channel_id'],
-        'start': 0
+        'message': "hello"
     })
     assert resp.status_code == 403
 
-def test_invalid_token_signature_channel_messages():
+def test_invalid_token_signature_message_send():
     requests.delete(config.url + 'clear/v1')
     #create user
     user = requests.post(config.url + 'auth/register/v2', json={
@@ -74,16 +77,16 @@ def test_invalid_token_signature_channel_messages():
     #create invalid token
     invalid_token = jwt.encode({'u_id': user_token['auth_user_id'], 'session_id': 0}, 'Invalid', algorithm='HS256')
     #pass valid channel id but invalid token
-    resp = requests.get(config.url + 'channel/messages/v2', params={
+    resp = requests.post(config.url + 'message/send/v1', params={
         'token': invalid_token, 
         'channel_id': channel_id['channel_id'],
-        'start': 0
+        'message': "hello"
     })
     assert resp.status_code == 403
 
-def test_channel_messages_route_works():
+def test_route_works_message_send():
     requests.delete(config.url + 'clear/v1')
-     #create a user
+
     user = requests.post(config.url + 'auth/register/v2', json={
         'email': 'validemail@gmail.com', 
         'password': '123abc!@#', 
@@ -91,26 +94,25 @@ def test_channel_messages_route_works():
         'name_last': 'Smith'
     })
     user_token = user.json()
-    #create a channel with that user
+
     channel = requests.post(config.url + 'channels/create/v2', json={
         'token': user_token['token'], 
         'name': 'Alpaca', 
         'is_public': True
     })
     channel_id = channel.json()
-    #pass in a start that is greater to the number of messages in the system
-    resp = requests.get(config.url + 'channel/messages/v2', params={
+
+    resp = requests.post(config.url + 'message/send/v1', params={
         'token': user_token['token'], 
         'channel_id': channel_id['channel_id'],
-        'start': 0,
+        'message': "hello",
     })
-    #get the response in json
+
     assert resp.status_code == 200
 
-#tests that there is a negative one when the end of a message is reached
-def test_negative_one_end_of_messages_channel_messages():
+def test_message_less_than_one_character_message_send():
     requests.delete(config.url + 'clear/v1')
-     #create a user
+
     user = requests.post(config.url + 'auth/register/v2', json={
         'email': 'validemail@gmail.com', 
         'password': '123abc!@#', 
@@ -118,47 +120,120 @@ def test_negative_one_end_of_messages_channel_messages():
         'name_last': 'Smith'
     })
     user_token = user.json()
-    #create a channel with that user
+
     channel = requests.post(config.url + 'channels/create/v2', json={
         'token': user_token['token'], 
         'name': 'Alpaca', 
         'is_public': True
     })
     channel_id = channel.json()
-    #pass in start when there are no more messages
-    resp = requests.get(config.url + 'channel/messages/v2', params={
+
+    resp = requests.post(config.url + 'message/send/v1', params={
         'token': user_token['token'], 
         'channel_id': channel_id['channel_id'],
-        'start': 0,
+        'message': ""
     })
-    #get the response in json
-    messages = resp.json()
-    assert messages['end'] == -1
 
-
-def test_start_greater_than_messages_channel_messages():
-    requests.delete(config.url + 'clear/v1')
-     #create a user
-    user = requests.post(config.url + 'auth/register/v2', json={
-        'email': 'validemail@gmail.com', 
-        'password': '123abc!@#', 
-        'name_first': 'Sam', 
-        'name_last': 'Smith'
-    })
-    user_token = user.json()
-    #create a channel with that user
-    channel = requests.post(config.url + 'channels/create/v2', json={
-        'token': user_token['token'], 
-        'name': 'Alpaca', 
-        'is_public': True
-    })
-    channel_id = channel.json()
-    #pass in a start that is greater to the number of messages in the system
-    resp = requests.get(config.url + 'channel/messages/v2', params={
-        'token': user_token['token'], 
-        'channel_id': channel_id['channel_id'],
-        'start': 1,
-    })
-    #get the response in json
     assert resp.status_code == 400
+
+def test_message_more_than_one_thousand_character_message_send():
+    requests.delete(config.url + 'clear/v1')
+
+    user = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'validemail@gmail.com', 
+        'password': '123abc!@#', 
+        'name_first': 'Sam', 
+        'name_last': 'Smith'
+    })
+    user_token = user.json()
+
+    channel = requests.post(config.url + 'channels/create/v2', json={
+        'token': user_token['token'], 
+        'name': 'Alpaca', 
+        'is_public': True
+    })
+    channel_id = channel.json()
+    
+    resp = requests.post(config.url + 'message/send/v1', params={
+        'token': user_token['token'], 
+        'channel_id': channel_id['channel_id'],
+        'message': "f" * 1001
+    })
+
+    assert resp.status_code == 400
+
+def test_check_message_ids_are_not_the_same_for_same_channel_message_send():
+    requests.delete(config.url + 'clear/v1')
+ 
+    user = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'validemail@gmail.com', 
+        'password': '123abc!@#', 
+        'name_first': 'Sam', 
+        'name_last': 'Smith'
+    })
+    user_token = user.json()
+
+    channel = requests.post(config.url + 'channels/create/v2', json={
+        'token': user_token['token'], 
+        'name': 'Alpaca', 
+        'is_public': True
+    })
+    channel_id = channel.json()
+
+    resp = requests.post(config.url + 'message/send/v1', params={
+        'token': user_token['token'], 
+        'channel_id': channel_id['channel_id'],
+        'message': "hello",
+    })
+    resp2 = requests.post(config.url + 'message/send/v1', params={
+        'token': user_token['token'], 
+        'channel_id': channel_id['channel_id'],
+        'message': "hello",
+    })
+
+    message1 = resp.json()
+    message2 = resp2.json()
+    assert message1['message_id'] != message2['message_id']
+
+def test_check_messagge_ids_are_not_the_same_for_different_channel_message_send():
+    requests.delete(config.url + 'clear/v1')
+
+    user = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'validemail@gmail.com', 
+        'password': '123abc!@#', 
+        'name_first': 'Sam', 
+        'name_last': 'Smith'
+    })
+    user_token = user.json()
+
+    channel = requests.post(config.url + 'channels/create/v2', json={
+        'token': user_token['token'], 
+        'name': 'Alpaca', 
+        'is_public': True
+    })
+    channel_id = channel.json()
+
+    channel2 = requests.post(config.url + 'channels/create/v2', json={
+        'token': user_token['token'], 
+        'name': 'Alpaca', 
+        'is_public': True
+    })
+    channel_id2 = channel2.json()
+    
+    resp = requests.post(config.url + 'message/send/v1', params={
+        'token': user_token['token'], 
+        'channel_id': channel_id['channel_id'],
+        'message': "hello",
+    })
+    resp2 = requests.post(config.url + 'message/send/v1', params={
+        'token': user_token['token'], 
+        'channel_id': channel_id2['channel_id'],
+        'message': "hello",
+    })
+
+    message1 = resp.json()
+    message2 = resp2.json()
+    assert message1['message_id'] != message2['message_id']
+
+test_check_messagge_ids_are_not_the_same_for_different_channel_and_dm_message_send()
 
