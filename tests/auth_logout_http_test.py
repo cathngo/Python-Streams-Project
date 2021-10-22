bdc101b2eea2f6db0f3e3be3e1ab918a45310194
Diwa <z5308320@cse.unsw.edu.au>
2021-10-23 03:11:44 +1100

    tests for logout

---
+++ tests/auth_logout_http_test.py
@@ -0,0 +1,57 @@
+import requests
+import jwt
+from src import config
+from src.config import SECRET
+
+# Checks if auth_logout_v2 works
+def test_http_auth_logout_works():
+    requests.delete(config.url + 'clear/v1')
+    r1 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'user1@email.com',
+        'password': 'user1password',
+        'name_first': 'Kanye',
+        'name_last': 'Yeezus',
+    })
+    payload1 = r1.json()
+    r2 = requests.post(config.url + 'auth/logout/v1', json={
+        'token': payload1['token'],
+    })
+    assert r2.status_code == 200
+
+def test_http_auth_logout_invalid_token():
+    requests.delete(config.url + 'clear/v1')
+    r1 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'user1@email.com',
+        'password': 'user1password',
+        'name_first': 'Kanye',
+        'name_last': 'Yeezus',
+    })
+    payload1 = r1.json()
+    invalid_token = jwt.encode({'u_id': 0, 'session_id': 0}, 'Invalid', algorithm='HS256')
+    r2 = requests.post(config.url + 'auth/logout/v1', json={
+        'token': invalid_token,
+    })
+    assert r2.status_code == 403
+
+def test_http_auth_double_logout():
+    requests.delete(config.url + 'clear/v1')
+    r1 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'user1@email.com',
+        'password': 'user1password',
+        'name_first': 'Kanye',
+        'name_last': 'Yeezus',
+    })
+    payload1 = r1.json()
+    r2 = requests.post(config.url + 'auth/logout/v1', json={
+        'token': payload1['token'],
+    })
+    r3 = requests.post(config.url + 'auth/logout/v1', json={
+        'token': payload1['token'],
+    })
+    assert r3.status_code == 403
+
+
+
+
+    
+
