9227fd589b81446ef7ec98738f303593718152ed
Minwook Han <z5259887@ad.unsw.edu.au>
2021-10-25 03:29:55 +1100

    Updated channel_join_helper.py for coverage

--- src/channel_join_helper.py
+++ src/channel_join_helper.py
@@ -1,14 +1,21 @@
 from src.error import InputError, AccessError
 def find_user(auth_user_id, store):
+    found = False
+    temp_user = {}
     for user in store['users']:
         if user['u_id'] == auth_user_id:
-            return user
+            temp_user = user
+            found = True
+    if found == True:
+        return temp_user
     raise AccessError
 
 def find_channel(channel_id, store):
+    temp_channel = {}
     for channel in store['channels']:
         if channel['channel_id'] == channel_id:
-            return channel
+            temp_channel = channel
+    return temp_channel
 
 def check_authorised_member(auth_user_id, channel_id, store):
     for channel in store['channels']:
@@ -17,5 +24,3 @@ def check_authorised_member(auth_user_id, channel_id, store):
                 if member['u_id'] == auth_user_id:
                     raise InputError
     return
-    
-
