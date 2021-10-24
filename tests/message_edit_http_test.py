8a2d2ef1f0d241ba6ebdbfc5dd7de43bd6023044
z5361577 <z5361577@cse.unsw.edu.au>
2021-10-25 04:16:02 +1100

    adding tests for message edit and message remove

--- tests/message_edit_http_test.py
+++ tests/message_edit_http_test.py
@@ -4,11 +4,9 @@ import json
 from src import config
 from src.other import clear_v1
 import jwt
-from src.data_store import data_store
 
-def test_valid_token_message_remove():
+def test_valid_token_message_edit():
     requests.delete(config.url + 'clear/v1')
-
     #register user 
     user = requests.post(config.url + 'auth/register/v2', json={
         'email': 'validemail@gmail.com', 
@@ -33,15 +31,16 @@ def test_valid_token_message_remove():
     message_id = message_send.json()
     #create invalid token
     invalid_token = jwt.encode({'u_id': user_token['auth_user_id'], 'session_id': 0}, 'Invalid', algorithm='HS256')
-    #remove message with the invalid token
-    removed = requests.delete(config.url + 'message/remove/v1', json={ 
+    #edit message with the invalid token
+    editd = requests.put(config.url + 'message/edit/v1', json={ 
         'token': invalid_token,
         'message_id': message_id,
+        'message':"goodbye"       
     })
     #get the response from the server for the message
-    assert removed.status_code == 403 
+    assert editd.status_code == 403 
 
-def test_message_id_not_in_dm_message_remove():
+def test_message_id_not_in_dm_message_edit():
     requests.delete(config.url + 'clear/v1')
     #register user 
     user = requests.post(config.url + 'auth/register/v2', json={
@@ -59,14 +58,15 @@ def test_message_id_not_in_dm_message_remove():
     })
     
     invalid_message_id = 1000
-    #remove message with the invalid message_id
-    removed = requests.delete(config.url + 'message/remove/v1', json={ 
+    #edit message with the invalid message_id
+    editd = requests.put(config.url + 'message/edit/v1', json={ 
         'token': user_token['token'],
         'message_id': invalid_message_id,
+        'message':"goodbye"
     })  
-    assert removed.status_code == 400 
+    assert editd.status_code == 400 
 
-def test_message_id_not_in_channel_message_remove():
+def test_message_id_not_in_channel_message_edit():
     requests.delete(config.url + 'clear/v1')
     #register user 
     user = requests.post(config.url + 'auth/register/v2', json={
@@ -84,14 +84,15 @@ def test_message_id_not_in_channel_message_remove():
     })
     
     invalid_message_id = 1000
-    #remove message with the invalid message_id
-    removed = requests.delete(config.url + 'message/remove/v1', json={ 
+    #edit message with the invalid message_id
+    editd = requests.put(config.url + 'message/edit/v1', json={ 
         'token': user_token['token'],
         'message_id': invalid_message_id,
+        'message':"goodbye"
     })  
-    assert removed.status_code == 400
+    assert editd.status_code == 400
 
-def test_member_did_not_send_the_dm_message_remove():
+def test_member_did_not_send_the_dm_message_edit():
 
     requests.delete(config.url + 'clear/v1')
     #register user 
@@ -123,15 +124,16 @@ def test_member_did_not_send_the_dm_message_remove():
         'message': "hello",
     })
     message_id = message_send.json()
-    #user2 attempts to remove message sent by user 1
-    removed = requests.delete(config.url + 'message/remove/v1', json={ 
+    #user2 attempts to edit message sent by user 1
+    editd = requests.put(config.url + 'message/edit/v1', json={ 
         'token': user2_token['token'],
         'message_id': message_id,
+        'message':"goodbye"
     })  
     #get the response from the server for the message
-    assert removed.status_code == 403 
+    assert editd.status_code == 403 
 
-def tests_member_message_was_removed_message_member_removed_channel():
+def tests_member_message_was_editd_message_member_editd_channel():
     requests.delete(config.url + 'clear/v1')
     #create a user
     user = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail@gmail.com', 
@@ -168,10 +170,11 @@ def tests_member_message_was_removed_message_member_removed_channel():
     })
     message_id = message_send.json()
     
-    #the channel owner removes the message   
-    requests.delete(config.url + 'message/remove/v1', json={ 
+    #the channel owner edits the message   
+    requests.put(config.url + 'message/edit/v1', json={ 
         'token': user_token2['token'],
-        'message_id': message_id['message_id'], 
+        'message_id': message_id['message_id'],
+        'message':"goodbye" 
     })  
 
     message_page = requests.get(config.url + 'channel/messages/v2', params={
@@ -181,9 +184,9 @@ def tests_member_message_was_removed_message_member_removed_channel():
     })
     message_list = message_page.json()
 
-    assert len(message_list['messages']) == 0
+    assert message_list['messages'][0]['message'] == "goodbye"
     
-def tests_member_message_was_removed_message_member_removed_dm():
+def tests_member_message_was_editd_message_member_editd_dm():
     requests.delete(config.url + 'clear/v1')
     #create a user
     user = requests.post(config.url + 'auth/register/v2', json={
@@ -218,10 +221,11 @@ def tests_member_message_was_removed_message_member_removed_dm():
     })
     message_id = message_send.json()
     
-    #the channel owner removes the message   
-    requests.delete(config.url + 'message/remove/v1', json={ 
+    #the channel owner edits the message   
+    requests.put(config.url + 'message/edit/v1', json={ 
         'token': user_token2['token'],
         'message_id': message_id['message_id'], 
+        'message': "goodbye"
     })  
 
     message_page = requests.get(config.url + 'dm/messages/v1', params={
@@ -231,9 +235,9 @@ def tests_member_message_was_removed_message_member_removed_dm():
     })
     message_list = message_page.json()
 
-    assert len(message_list['messages']) == 0
+    assert message_list['messages'][0]['message'] == "goodbye"
 
-def test_owner_can_remove_message_from_member_channel_message_remove():  
+def test_owner_can_edit_message_from_member_channel_message_edit():  
     requests.delete(config.url + 'clear/v1')
     #create a user
     user = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail@gmail.com', 
@@ -270,23 +274,23 @@ def test_owner_can_remove_message_from_member_channel_message_remove():
     })
     message_id = message_send.json()
     
-    #the channel owner removes the message   
-    requests.delete(config.url + 'message/remove/v1', json={ 
+    #the channel owner edits the message   
+    requests.put(config.url + 'message/edit/v1', json={ 
         'token': user_token['token'],
-        'message_id': message_id['message_id'], 
+        'message_id': message_id['message_id'],
+        'message': "goodbye" 
     })  
 
     message_page = requests.get(config.url + 'channel/messages/v2', params={
         'token': user_token2['token'], 
         'channel_id': channel_id['channel_id'],
-        'start': 0,
+        'start': 0
     })
     message_list = message_page.json()
 
-    assert len(message_list['messages']) == 0
+    assert message_list['messages'][0]['message'] == "goodbye"
 
-def test_member_message_was_removed_by_owner_removed_dm():
-    
+def test_member_message_was_editd_by_owner_editd_dm():  
     requests.delete(config.url + 'clear/v1')
     #create a user
     user = requests.post(config.url + 'auth/register/v2', json={
@@ -321,10 +325,11 @@ def test_member_message_was_removed_by_owner_removed_dm():
     })
     message_id = message_send.json()
     
-    #the channel owner removes the message   
-    requests.delete(config.url + 'message/remove/v1', json={ 
+    #the channel owner edits the message   
+    requests.put(config.url + 'message/edit/v1', json={ 
         'token': user_token['token'],
-        'message_id': message_id['message_id'], 
+        'message_id': message_id['message_id'],
+        'message': "goodbye"
     })  
 
     message_page = requests.get(config.url + 'dm/messages/v1', params={
@@ -334,4 +339,107 @@ def test_member_message_was_removed_by_owner_removed_dm():
     })
     message_list = message_page.json()
 
-    assert len(message_list['messages']) == 0
+    assert message_list['messages'][0]['message'] == "goodbye"
+
+def tests_member_message_was_deleted_by_empty_string_message_member_edit_dm():
+    requests.delete(config.url + 'clear/v1')
+    #create a user
+    user = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'validemail@gmail.com', 
+        'password': '123abc!@#', 
+        'name_first': 'Sam', 
+        'name_last': 'Smith'
+    })
+    user_token = user.json()
+
+    #create a user
+    user2 = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'validemail1@gmail.com', 
+        'password': '122abc!@#', 
+        'name_first': 'Tam', 
+        'name_last': 'Lam'
+    })
+    user_token2 = user2.json()
+
+    #create a dm with those users
+    dm = requests.post(config.url + 'dm/create/v1', json={
+        'token': user_token['token'], 
+        'u_ids': [user_token2['auth_user_id']],
+    })
+    dm_id = dm.json()
+    #the member user sends a message to channel
+    
+    message_send = requests.post(config.url + 'message/senddm/v1', json={
+        'token': user_token2['token'], 
+        'dm_id': dm_id['dm_id'],
+        'message': "hello",
+    })
+    message_id = message_send.json()
+    
+    #the channel owner edits the message   
+    requests.put(config.url + 'message/edit/v1', json={ 
+        'token': user_token2['token'],
+        'message_id': message_id['message_id'], 
+        'message': ""
+    })  
+
+    message_page = requests.get(config.url + 'dm/messages/v1', params={
+        'token': user_token2['token'], 
+        'dm_id': dm_id['dm_id'],
+        'start': 0,
+    })
+    message_list = message_page.json()
+    assert (message_list['messages']) == []
+
+def tests_member_message_was_deleted_by_empty_string_message_member_edit_channel():
+    requests.delete(config.url + 'clear/v1')
+    #create a user
+    user = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail@gmail.com', 
+        'password': '123abc!@#', 
+        'name_first': 'Sam', 
+        'name_last': 'Smith'
+    })
+    user_token = user.json()
+    #create a channel with that user
+    channel = requests.post(config.url + 'channels/create/v2', json={
+        'token': user_token['token'], 
+        'name': 'Alpaca', 
+        'is_public': True
+    })
+    channel_id = channel.json()
+    #create a user
+    user2 = requests.post(config.url + 'auth/register/v2', json={'email': 'validemail1@gmail.com', 
+        'password': '122abc!@#', 
+        'name_first': 'Tam', 
+        'name_last': 'Lam'
+    })
+    user_token2 = user2.json()
+    
+    requests.post(config.url + 'channel/join/v2', json={
+        'token': user_token2['token'], 
+        'channel_id': channel_id['channel_id']
+    })
+    
+    #the member user sends a message to channel
+    message_send = requests.post(config.url + 'message/send/v1', json={
+        'token': user_token2['token'], 
+        'channel_id': channel_id['channel_id'],
+        'message': "hello",
+    })
+    message_id = message_send.json()
+    
+    #the channel owner edits the message   
+    requests.put(config.url + 'message/edit/v1', json={ 
+        'token': user_token2['token'],
+        'message_id': message_id['message_id'],
+        'message':"" 
+    })  
+
+    message_page = requests.get(config.url + 'channel/messages/v2', params={
+        'token': user_token2['token'], 
+        'channel_id': channel_id['channel_id'],
+        'start': 0,
+    })
+    message_list = message_page.json()
+
+    assert (message_list['messages']) == []
