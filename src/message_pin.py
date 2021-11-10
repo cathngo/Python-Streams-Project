ff53e732d3ef2b771517c06c9120b1aefd4c217e
z5361577 <z5361577@cse.unsw.edu.au>
2021-11-10 23:31:45 +1100

    added dosctring

--- src/message_pin.py
+++ src/message_pin.py
@@ -37,6 +37,24 @@ Return Value:
     raise InputError(description= "message_id is not valid")
 
 def message_unpin_v1(u_id, message_id):
+    '''
+Given a message within a channel or DM, remove its mark as pinned.
+
+Arguments:
+    u_id (int) - the id of the authorised user that is trying to unpin the message
+    message_id (int) - the id of the message that the user is trying to unpin
+
+Exceptions:
+    InputError - Occurs when any of:
+        -message_id is not already pinned
+    AccessError - Occurs when any of :
+        - An invalid token was passed to the route
+        - message_id refers to a valid message in a joined channel/DM and 
+        the authorised user does not have owner permissions in the channel/DM
+
+Return Value: 
+    Returns an empty dictionary if the message is successfully unpinned
+'''
     in_channel_found = in_channel_search(message_id)
     in_dm_found = in_dm_search(message_id)
 
