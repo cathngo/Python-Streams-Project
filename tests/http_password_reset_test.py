006c2de6895d325cf9202b08b33efef159a2d215
Alejandro Stevan <z5295949@cse.unsw.edu.au>
2021-11-11 08:12:08 +1100

    Fixtures fixes

--- tests/http_password_reset_test.py
+++ tests/http_password_reset_test.py
@@ -4,12 +4,10 @@ from src.error import InputError, AccessError
 from tests.pytest_fixtures import clear, reg_user_alpaca, reg_user2, reg_user1
 
 def test_invalid_code(clear, reg_user_alpaca):
-    reg_user_alpaca
     re = requests.post(config.url + 'auth/passwordreset/reset/v1', json={'reset_code': 'invalid_code','new_password': 'validpassword'})
     assert re.status_code == InputError.code
 
 def test_invalid_password(clear, reg_user1):
-    reg_user1
     re = requests.post(config.url + 'auth/passwordreset/reset/v1', json={'reset_code': 'invalid_code','new_password': 'inval'})
     assert re.status_code == InputError.code
 
