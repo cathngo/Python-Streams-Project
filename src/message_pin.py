eb62918af7ea159fd03581a423aa4e7899570345
z5361577 <z5361577@cse.unsw.edu.au>
2021-11-10 22:43:16 +1100

    Finished the application for message unpin

--- src/message_pin.py
+++ src/message_pin.py
@@ -1,7 +1,7 @@
 from src.data_store import data_store
 from src.error import InputError
 from src.remove_edit_message_helper import in_channel_search, in_dm_search
-from src.message_pin_helper import pin_dm_message, pin_channel_message
+from src.message_pin_helper import pin_dm_message, pin_channel_message, unpin_channel_message, unpin_dm_message
 
 def message_pin_v1(u_id, message_id):
     '''
@@ -37,4 +37,15 @@ Return Value:
     raise InputError(description= "message_id is not valid")
 
 def message_unpin_v1(u_id, message_id):
-    pass
+    in_channel_found = in_channel_search(message_id)
+    in_dm_found = in_dm_search(message_id)
+
+    if in_channel_found:
+        unpin_channel_message(u_id, message_id, in_channel_found['message'], in_channel_found['channel'])
+        return {}
+    
+    if in_dm_found:
+        unpin_dm_message(u_id, message_id, in_dm_found['message'], in_dm_found['dm'])
+        return {}
+
+    raise InputError(description= "message_id is not valid")
